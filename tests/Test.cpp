c83e88b774a1b70dc57fdd71ad947893e4f8f0bf
cyj <2104067267@qq.com>
2024-11-23 22:37:24 +0800

    4

--- tests/Test.cpp
+++ tests/Test.cpp
@@ -26,6 +26,14 @@ TEST(ExecutorTest, should_return_facing_N_given_command_is_L_and_facing_is_E)
     ASSERT_EQ(target, executor->Query());
 }
 
+TEST(ExecutorTest, should_return_facing_E_given_command_is_L_and_facing_is_S)
+{
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'S'}));
+    executor->Execute("L");
+    const Pose target({0, 0,'E'});
+    ASSERT_EQ(target, executor->Query());
+}
+
 TEST(ExecutorTest,should_return_facing_S_given_command_is_R_and_facing_is_W)
 {
     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'W'}));
@@ -55,6 +63,22 @@ TEST(ExecutorTest,should_return_1_0_given_command_is_M_and_facing_is_E)
     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
     executor->Execute("M");
     const Pose target({1, 0, 'E'});
+    ASSERT_EQ(target, executor->Query());
+}
+
+TEST(ExecutorTest,should_return_facing_E1n2_given_command_is_RMRM_and_facing_is_N)
+{
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'N'}));
+    executor->Execute("RMRMML");
+    const Pose target({1, -2, 'E'});
+    ASSERT_EQ(target, executor->Query());
+}
+
+TEST(ExecutorTest,should_return_facing_S1n2_given_command_is_RMRM_and_facing_is_N)
+{
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'N'}));
+    executor->Execute("MRMMR");
+    const Pose target({2, 1, 'S'});
     ASSERT_EQ(target, executor->Query());
 }
 
